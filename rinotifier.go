176034c580adcee042487f20d1150bf6b2f78638
Chris Geisel <geiselc@amazon.com>
2018-10-24 22:37:54 -0700

    WIP segfault on sqs return

--- rinotifier.go
+++ rinotifier.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-  // "github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-  // "log"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"time"
 )
+
+type ReservedInstance struct {
+	End string
+}
 
 func main() {
 	sess, err := session.NewSession(&aws.Config{
@@ -23,31 +27,66 @@ func main() {
 		fmt.Println("Could not get credentials, exiting", err)
 		return
 	}
-  //
-	// sess.Handlers.Send.PushFront(func(r *request.Request) {
-	// 	// Log every request made and its payload
-	// 	log.Println("Request: %s/%s, Payload: %s", r.ClientInfo.ServiceName, r.Operation, r.Params)
-	// })
 
 	// svc := ec2.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
-  svc := ec2.New(sess)
+	svc := ec2.New(sess)
 
-  params := &ec2.DescribeInstancesInput{
+	sqs_svc := sqs.New(sess)
+
+	// URL to our queue
+	daily_qURL := "QueueURL"
+	weekly_qURL := "QueueURL"
+
+	params := &ec2.DescribeReservedInstancesInput{
 		Filters: []*ec2.Filter{
 			{
-				Name:   aws.String("tag:Environment"),
-				Values: []*string{aws.String("prod")},
-			},
-			{
-				Name:   aws.String("instance-state-name"),
-				Values: []*string{aws.String("running"), aws.String("pending")},
+				Name:   aws.String("state"),
+				Values: []*string{aws.String("active")},
 			},
 		},
 	}
 
-	subs, err := svc.DescribeReservedInstances(params)
+	active_ris, err := svc.DescribeReservedInstances(params)
 	if err != nil {
 		fmt.Println("Could not DescribeReservedInstances", err)
 	}
-	fmt.Println("Reserved instances", subs)
+	for _, ri := range active_ris.ReservedInstances {
+
+		if isExpiring(ri, 7) {
+			q, err := addToQueue(sqs_svc, daily_qURL, ri)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(q)
+		}
+
+		if isExpiring(ri, 365) {
+			q, err := addToQueue(sqs_svc, weekly_qURL, ri)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(q)
+		}
+	}
+}
+
+func isExpiring(ri *ec2.ReservedInstances, days int) bool {
+	const ri_form = "2006-01-02 15:04:05 +0000"
+	return ri.End.Before(time.Now().Add(time.Duration(days*24) * time.Hour))
+}
+
+func addToQueue(svc *sqs.SQS, qURL string, ri *ec2.ReservedInstances) (string, error) {
+	result, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Account": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String("The Whistler"),
+			},
+		},
+		MessageBody: aws.String(ri.End.String()),
+		QueueUrl:    &qURL,
+	})
+
+	return *result.MessageId, err
 }

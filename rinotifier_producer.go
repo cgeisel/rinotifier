e7e68386b6b1fcdcb02ae99d42c4b9e83a7e55c0
Chris Geisel <geiselc@amazon.com>
2019-01-02 15:29:09 -0800

    added lambda handler, adding multiple RIs to message, debug logging

--- rinotifier_producer.go
+++ rinotifier_producer.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,9 +18,9 @@ type ReservedInstance struct {
 	End string
 }
 
-func main() {
+func riHandler() {
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2"),
+		Region: aws.String(os.Getenv("AWS_REGION")),
 	})
 	if err != nil {
 		log.Println("Could not create session, exiting", err)
@@ -28,14 +33,20 @@ func main() {
 		return
 	}
 
-	svc := ec2.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
-	///svc := ec2.New(sess)
+	var svc *ec2.EC2
+	var sqs_svc *sqs.SQS
 
-	sqs_svc := sqs.New(sess)
+	debug, err := strconv.ParseBool(os.Getenv("DEBUG"))
+	if err != nil || !debug {
+		svc = ec2.New(sess)
+		sqs_svc = sqs.New(sess)
+	} else {
+		svc = ec2.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
+		sqs_svc = sqs.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
+	}
 
 	// URL to our queue
-	// daily_qURL := "https://sqs.us-west-2.amazonaws.com/268368477672/rinotifier-test"
-	weekly_qURL := "https://sqs.us-west-2.amazonaws.com/268368477672/rinotifier-test"
+	qURL := os.Getenv("QUEUE_URL")
 
 	params := &ec2.DescribeReservedInstancesInput{
 		Filters: []*ec2.Filter{
@@ -48,18 +59,24 @@ func main() {
 
 	active_ris, err := svc.DescribeReservedInstances(params)
 	if err != nil {
-		log.Println("Could not DescribeReservedInstances", err)
+		log.Println("Could not DescribeReservedInstances, exiting", err)
+		return
 	}
-	for i, ri := range active_ris.ReservedInstances {
+
+	var expiring_ris []*ec2.ReservedInstances
+	for _, ri := range active_ris.ReservedInstances {
 		if isExpiring(ri, 365) {
-			// log.Println(ri)
-			q, err := addToQueue(sqs_svc, weekly_qURL, ri)
-			if err != nil {
-				log.Println(err)
-			}
-			log.Println(i)
-			log.Println(q)
+			expiring_ris = append(expiring_ris, ri)
 		}
+	}
+	fmt.Println(expiring_ris)
+	if len(expiring_ris) > 0 {
+		q, err := addToQueue(sqs_svc, qURL, expiring_ris)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(q)
 	}
 }
 
@@ -68,12 +85,21 @@ func isExpiring(ri *ec2.ReservedInstances, days int) bool {
 	return ri.End.Before(time.Now().Add(time.Duration(days*24) * time.Hour))
 }
 
-func addToQueue(svc *sqs.SQS, qURL string, ri *ec2.ReservedInstances) (string, error) {
+func addToQueue(svc *sqs.SQS, qURL string, ri []*ec2.ReservedInstances) (string, error) {
+	json_ri, err := json.Marshal(ri)
+	if err != nil {
+		log.Println("Could not marshal json, exiting", err)
+		return "Could not marshal json", err
+	}
 	result, err := svc.SendMessage(&sqs.SendMessageInput{
 		DelaySeconds: aws.Int64(10),
-		MessageBody:  aws.String(ri.String()),
+		MessageBody:  aws.String(string(json_ri)),
 		QueueUrl:     &qURL,
 	})
 
 	return *result.MessageId, err
+}
+
+func main() {
+	lambda.Start(riHandler)
 }
